257b111e8252135295dfd6d0b571651dd51bfbc9
Pratap Neaupane <sereversion@gmail.com>
2020-05-04 11:33:29 +0545

    created ball changeable

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -27,10 +29,17 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset('images/ball1.png'),
+      child: FlatButton(
+        onPressed: () {
+          ballNumber = Random().nextInt(5) + 1;
+          print(ballNumber);
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
     );
   }
 }
