6d60a7542adf642394cd6de83b46baefaca289bb
Pratap Neaupane <sereversion@gmail.com>
2020-05-04 11:24:57 +0545

    created statefull widget for Ball

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: BallPage(),
       ),
     );
@@ -12,14 +13,24 @@ class BallPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue.shade900,
-        title: Title(
-          color: null,
-          child: Text('Ask Me Anything'),
-        ),
+        title: Text('Ask Me Anything'),
       ),
-      body: Container(
-        color: Colors.blue,
-      ),
+      body: Ball(),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
     );
   }
 }
